ea96b3c64e2b4bd9f09f36e5ba6e5d660fd8078c
jahnvi <jahnvigupta11@gmail.com>
2020-10-14 23:10:32 +0530

    Added main.c and veclib.h

---
+++ vecmain.c
@@ -0,0 +1,25 @@
+#include "veclib.h"
+
+//Function to print the vector
+void print(vector vec) {
+	int i;
+	printf("[");
+	for (i=0;i<DIM;i++)
+		printf("%f ",vec.x[i]);
+	printf("]");
+}
+
+int main() {
+	vector vec1, vec2;
+	printf("Enter the elements of first vector: \n");
+	for(int i=0;i<DIM;i++)
+		scanf("%f",&vec1.x[i]);
+	printf("Enter the elements of second vector: \n");
+	for(int i=0;i<DIM;i++)
+		scanf("%f",&vec2.x[i]);	
+	vector vec_sum = add(vec1,vec2);
+	printf("Sum of vectors: \n");
+	print(vec_sum);
+	printf("\n");
+}
+
